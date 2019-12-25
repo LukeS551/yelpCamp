4945f8ca264e432a210f579c5a404d64871760bd
Luke <LSSpencer551@gmail.com>
2019-12-26 12:29:55 +1300

    location page added

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require("express");
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
-var app = express();
+const app = express();
+
+
+app.set("view engine", "ejs");
 app.get('/', function(req, res){
-    res.send("this will be the landing page soon!");
+    res.render("landing");
+});
+app.use(express.static('resources'));
+
+app.get("/locations", function(req, res){
+    const locations = [
+        {name: "Auckland", image: "/images//dan-freeman-hIKVSVKH7No-unsplash.jpg" },
+        {name: "Matamata", image: "/images/andres-iga-7XKkJVw1d8c-unsplash.jpg" },
+    ]
+    res.render("locations", {locations:locations});
 });
 
 app.listen(port, hostname, () => {
